cb314af9dfe0fb8d053d1e0ed99ab9f41d9e39c4
Loc Tran <xloctran@gmail.com>
2021-07-30 16:48:10 +0200

    add function to purity function

--- R/supercell_purity.R
+++ R/supercell_purity.R
@@ -3,7 +3,7 @@
 #'
 #' @param clusters vector of clustering assignment (reference assignment)
 #' @param supercell_membership vector of assignment of single-cell data to super-cells (membership field of \code{\link{SCimplify}} function output)
-#'
+#' @param ge gene expression use to compute the purity based on gene expression values
 #'
 #' @return a vector of super-cell purity, which is defined as a proportion of the most abundant cluster within super-cell.
 #' With 1 meaning that super-cell consists of single cells from one cluster (reference assignment)
@@ -11,17 +11,21 @@
 #' @export
 #'
 
-supercell_purity <- function(clusters, supercell_membership){
-  cl.gr            <- table(clusters, supercell_membership)
-  cluster.size     <- as.numeric(table(clusters))
-  group.size       <- as.numeric(table(supercell_membership))
+supercell_purity <- function(ge = NULL, clusters=NULL, supercell_membership){
+  if (!is.na(cluster)){
+    cl.gr            <- table(clusters, supercell_membership)
+    cluster.size     <- as.numeric(table(clusters))
+    group.size       <- as.numeric(table(supercell_membership))
 
-  Ng               <- length(group.size)
-  group.max.cl     <- rep(0, Ng)
+    Ng               <- length(group.size)
+    group.max.cl     <- rep(0, Ng)
 
 
-  cl.gr            <- sweep(cl.gr, 2, group.size, "/")
+    cl.gr            <- sweep(cl.gr, 2, group.size, "/")
 
-  res              <- apply(cl.gr, 2, max)
+    res              <- apply(cl.gr, 2, max)
+  } else {
+    res <- rep(1, ncol(ge))
+  }
   return(res)
 }
