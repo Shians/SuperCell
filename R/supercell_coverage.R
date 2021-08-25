1c5e9e27807d41de7e7db280c0e36f22639b14a1
Loc Tran <xloctran@gmail.com>
2021-08-25 16:20:59 +0200

    add labels to coverage purity plot

--- R/supercell_coverage.R
+++ R/supercell_coverage.R
@@ -7,7 +7,6 @@
 supercell_NonZero <- function(GE, membership, genes){
   coverage_sc <- Matrix::rowSums(1*(GE[genes, ] != 0))
   GE_SC <- supercell_GE(GE, membership)
-  coverage_SC <- Matrix::rowSums(1*(GE_SC[genes, ] != 0))
-  increase <- mean(coverage_SC/coverage_sc)
-  return(increase)
+  coverage_SC <- Matrix::rowSums(1*(GE_SC[genes, ] != 0))/dim(GE_SC)[2]
+  return(coverage_SC)
 }
